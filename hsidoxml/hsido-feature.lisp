d538716dcd17050c5d7a545b9fc29f0d0f1040f8
jumpei <junnpit@gmail.com>
2014-04-22 11:33:21 +0900

    addition: hsidoxml/hsido-feature.lisp

---
+++ hsidoxml/hsido-feature.lisp
@@ -0,0 +1,20 @@
+(in-package :hx)
+
+(defvar *xmldir* (cl-fad:pathname-as-directory
+		  (merge-pathnames ksetting::*hsido-directory*
+				   "xml")))
+
+;; f:/zip/MAIN/HSIDO/xml/g26109039462013100702000001.xml
+(defun xml-contents (xmlname)
+  (cxml:parse-file xmlname (stp:make-builder)))
+
+(defmacro has-predicate (xmlname code)
+  `(not
+    (xpath:node-set-empty-p
+     (kxml:xpath #'identity (xml-contents ,xmlname)
+		 "<cs>" ,(format nil "code[@code='~A']" code)))))
+
+(defun has-first?     (xmlname) (has-predicate xmlname "90030"))
+(defun has-continual? (xmlname) (has-predicate xmlname "90040"))
+(defun has-internal?  (xmlname) (has-predicate xmlname "90050"))
+(defun has-final?     (xmlname) (has-predicate xmlname "90060"))
